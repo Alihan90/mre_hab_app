499989824b2cb688d158094f9c801e8161f88e1c
Alihan90 <137628088+Alihan90@users.noreply.github.com>
2026-06-08 15:28:56 +0300

    Update irp_global_screen.dart

--- lib/screens/irp_global_screen.dart
+++ lib/screens/irp_global_screen.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import '../models.dart';
+import 'package:flutter/material.dart';
 
 class IrpGlobalScreen extends StatefulWidget {
   final List<Patient> patients;
